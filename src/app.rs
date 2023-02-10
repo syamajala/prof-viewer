38fbaa01a4deb37109b0ecfdf7c08a99fedd4945
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-02-10 13:00:28 -0800

    Light theme by default.

--- src/app.rs
+++ src/app.rs
@@ -826,6 +826,8 @@ impl ProfApp {
         // This is also where you can customized the look at feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
+        cc.egui_ctx.set_visuals(egui::Visuals::light());
+
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         let mut result: Self = if let Some(storage) = cc.storage {
