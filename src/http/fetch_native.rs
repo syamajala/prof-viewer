0ce1ba5e03a63814071790d8a78a868bc46d4f33
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-06-11 15:15:40 -0700

    Use rayon thread pool for spawning requests on native.

--- src/http/fetch_native.rs
+++ src/http/fetch_native.rs
@@ -6,16 +6,13 @@ pub fn fetch(
     request: RequestBuilder,
     on_done: Box<dyn FnOnce(Result<DataSourceResponse, String>) + Send>,
 ) {
-    std::thread::Builder::new()
-        .name("ehttp".to_owned())
-        .spawn(move || {
-            let text = request
-                .send()
-                .expect("test")
-                .text()
-                .expect("unable to get text");
+    rayon::spawn(move || {
+        let text = request
+            .send()
+            .expect("test")
+            .text()
+            .expect("unable to get text");
 
-            on_done(Ok(DataSourceResponse { body: text }))
-        })
-        .expect("unable to spawn thread");
+        on_done(Ok(DataSourceResponse { body: text }))
+    });
 }
